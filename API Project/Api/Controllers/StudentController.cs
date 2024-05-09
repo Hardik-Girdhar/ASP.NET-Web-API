d332042fb1ac10141e2280e90252e0e7cc2e0f90
Hardik <103685871+Hardik-Girdhar@users.noreply.github.com>
2024-05-09 16:35:18 +0530

    Add files via upload

---
+++ API Project/Api/Controllers/StudentController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        static List<Student> students = new List<Student>();
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return students;
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return students.FirstOrDefault(s => s.Id == id);
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] Student value)
+        {
+            students.Add(value);    
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Student value)
+        {
+            int i = students.FindIndex(s => s.Id == id);
+            if(i >= 0)
+            {
+                students[i] = value;
+            }
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            students.RemoveAll(s => s.Id == id);
+        }
+    }
+}
